bd6fe599cff7a58776965a50aa7419b796b41f9b
twiney94 <mp482a@otmail.fr>
2022-02-03 14:03:22 +0100

    :bricks: Added MVC default files/folder

---
+++ www/conf.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+
+define("DBNAME","sported");
+define("DBUSER","root");
+define("DBPWD","password");
+define("DBDRIVER","mysql");
+define("DBPORT","3306");
+define("DBHOST","database");
+// define("DBPREFIXE","esgi_");
