e3c96abff86eb2976bef955fbe974ed0facd30ef
Yass <mp482a@hotmail.fr>
2022-05-20 15:11:15 +0200

    :sparkles: PHPMailer function via Sported SMTP

--- www/Controller/Mail.class.php
+++ www/Controller/Mail.class.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Core\View;
+use App\Model\Register;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -13,46 +14,16 @@ require 'PHPMailer/src/SMTP.php';
 class Mail
 {
 
-    public function mail()
+    public function verificationMail()
     {
-        // Enable view testmail.view.php
         $view = new View("testmail");
-        $view->assign("titleSeo","Se connecter au site");
-    }
 
-    public function connectSMTP()
-    {
+        $registration = new Register;
 
-        $mail = new PHPMailer();
-        $mail->IsSMTP();
-        $mail->Host = 'localhost';               //Adresse IP ou DNS du serveur SMTP
-        $mail->Port = 465;                          //Port TCP du serveur SMTP
-        $mail->SMTPAuth = 1;                        //Utiliser l'identification
+        $view->assign("titleSeo", "Test mail");
 
-        if ($mail->SMTPAuth) {
-            $mail->SMTPSecure = 'ssl';               //Protocole de sécurisation des échanges avec le SMTP
-            $mail->Username   =  'noreply@sported.site';   //Adresse email à utiliser
-            $mail->Password   =  'password';         //Mot de passe de l'adresse email à utiliser
-        }
-
-        $mail->smtpConnect();
-    }
-
-    public function sendMail($mail, $Use_HTML) 
-    {
-        $mail->From       =  'contact@ovh.net';                //L'email à afficher pour l'envoi
-        $mail->FromName   = 'Contact de ovh.net';             //L'alias à afficher pour l'envoi
-
-        $mail->Subject    =  'Mon sujet';                      //Le sujet du mail
-        $mail->WordWrap   = 50;                                //Nombre de caracteres pour le retour a la ligne automatique
-        $mail->AltBody = 'Mon message en texte brut';            //Texte brut
-        $mail->IsHTML(false);                                  //Préciser qu'il faut utiliser le texte brut
-
-        if ($Use_HTML == true) {
-            $mail->MsgHTML('<div>Mon message en <code>HTML</code></div>');                         //Le contenu au format HTML
-            $mail->IsHTML(true);
-        }
-
-        $view = new View("testmail");
+        $token = "abcde";
+        $email = "mail@adress.com";
+        $registration->sendMail($email, $token);
     }
 }
