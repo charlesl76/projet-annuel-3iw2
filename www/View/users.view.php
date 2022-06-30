58f0e3fd6feafcde2d3d4949f07f5b9692d84c7d
Alexis Lin <alexislinscolaire@hotmail.com>
2022-06-30 02:03:31 +0200

    :art: Add DataTable for users pages

--- www/View/users.view.php
+++ www/View/users.view.php
@@ -1,12 +1,17 @@
 <h1>Liste des utilisateurs</h1>
 
-<table>
+<table id="myTable" class="display">
+    <thead>
+        <tr>
+            <td>Username</td>
+            <td>Firstname</td>
+            <td>Lastname</td>
+            <td>Role</td>
+            <td colspan=2 >Actions</td>
+        </tr>
+    </thead>
 
-    <td>Username</td>
-    <td>Firstname</td>
-    <td>Lastname</td>
-    <td>Role</td>
-
+    <tbody>
         <?php foreach($users as $user) : ?> 
             <tr>
                 <td><?php echo $user['username']; ?> </td>
@@ -25,5 +30,24 @@
 
             </tr>
         <?php endforeach; ?>
-
+    </tbody>
 </table>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#myTable').DataTable({
+            "paging": false,
+            "columns": [{
+                    "width": "5%"
+                },
+                null,
+                null,
+                null,
+                null
+            ],
+        });
+    });
+</script>
