089d69d95dc6567c29b222bf01e95c5fd043e308
Yass <mp482a@hotmail.fr>
2022-07-07 14:35:23 +0200

    :construction: Setting up view and updated model/controller

---
+++ www/View/tags.view.php
@@ -0,0 +1,30 @@
+<h1>Tags</h1>
+
+<?php
+
+if ((isset($_POST['action']) && $_POST['action'] !== null)) :
+    switch ($_POST['action']):
+        case 'create':
+            $view->create = 'create';
+            $view->includePartial("form", $tag->getFormTags());
+            break;
+        case 'update':
+            break;
+        case 'delete':
+            break;
+    endswitch;
+elseif (isset($action) && $action !== null) :
+    switch ($action):
+        case 'update':
+            $view->update = 'update';
+            $view->includePartial("form", $tag->getFormUpdateTags($postById));
+    
+            break;
+        case 'delete':
+            break;
+    endswitch;
+else :
+    $view->includePartial("posts", $tag->getAllTags());
+endif;
+
+?>
