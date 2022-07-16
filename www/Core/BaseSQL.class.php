46c84721f1144745df7afb95c9aaaf0c7636e8c1
douniaharag <douniaharagsio@gmail.com>
2022-07-16 15:00:54 +0200

    :wrench: Update BaseSql

--- www/Core/BaseSQL.class.php
+++ www/Core/BaseSQL.class.php
@@ -2,6 +2,8 @@
 
 namespace App\Core;
 
+use App\Core\Db;
+//use PDO;
 
 abstract class BaseSQL
 {
@@ -12,28 +14,38 @@ abstract class BaseSQL
     public function __construct()
     {
         //Faudra intégrer le singleton
-        try{
+
+        $this->pdo = Db::connect();
+
+        /*try {
             //Connexion à la base de données
-            $this->pdo = new \PDO( DBDRIVER.":host=".DBHOST.";port=".DBPORT.";dbname=".DBNAME ,DBUSER , DBPWD );
+            $this->pdo = new \PDO(DBDRIVER . ":host=" . DBHOST . ";port=" . DBPORT . ";dbname=" . DBNAME, DBUSER, DBPWD);
             $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        }catch(\Exception $e){
-            die("Erreur SQL".$e->getMessage());
+        } catch (\Exception $e) {
+            die("Erreur SQL" . $e->getMessage());
+
         }
+        */
+
+
         //Récupérer le nom de la table :
         // -> prefixe + nom de la classe enfant
-        $classExploded = explode("\\",get_called_class());
-        $this->table = DBPREFIXE.strtolower(end($classExploded));
-
+        $classExploded = explode("\\", get_called_class());
+        $this->table = DBPREFIXE . strtolower(end($classExploded));
     }
 
     /**
      * @param mixed $id
      */
-    public function setId($id): object
+    public function setId($id)
     {
-        $sql = "SELECT * FROM ".$this->table. " WHERE id=:id ";
-        $queryPrepared = $this->pdo->prepare($sql);
-        $queryPrepared->execute( ["id"=>$id] );
+        $sql = "SELECT * FROM " . $this->table . " WHERE id=:id ";
+
+        $queryPrepared = $this->pdo->prepare($sql, ['id' => $id]);
+
+       /* $queryPrepared = $this->pdo->prepare($sql);
+        $queryPrepared->execute(["id" => $id]);*/
+
         return $queryPrepared->fetchObject(get_called_class());
     }
 
@@ -46,22 +58,63 @@ abstract class BaseSQL
         $columns = array_diff_key($columns, $varsToExclude);
         $columns = array_filter($columns);
 
+        if (!is_null($this->getId())) {
+            foreach ($columns as $key => $value) {
+                $setUpdate[] = $key . "=:" . $key;
+            }
+            $sql = "UPDATE " . $this->table . " SET " . implode(",", $setUpdate) . " WHERE id=" . $this->getId();
+        } else {
+            $sql = "INSERT INTO " . $this->table . " (" . implode(",", array_keys($columns)) . ")
+            VALUES (:" . implode(",:", array_keys($columns)) . ")";
+        }
 
-       if( !is_null($this->getId()) ){
-           foreach ($columns as $key=>$value){
-                $setUpdate[]=$key."=:".$key;
-           }
-           $sql = "UPDATE ".$this->table." SET ".implode(",",$setUpdate)." WHERE id=".$this->getId();
-       }else{
-            $sql = "INSERT INTO ".$this->table." (".implode(",", array_keys($columns)).")
-            VALUES (:".implode(",:", array_keys($columns)).")";
-       }
+        /*$queryPrepared = $this->pdo->prepare($sql);
+        $queryPrepared->execute($columns);*/
+        $this->pdo->prepare($sql, $colums);
+    }
+
+  
+    public function findAll()
+    {
+        $sql = "SELECT * FROM ".$this->table;
 
         $queryPrepared = $this->pdo->prepare($sql);
-        $queryPrepared->execute( $columns );
+        $queryPrepared->execute();
 
+        return $queryPrepared->fetchAll(PDO::FETCH_ASSOC);
+    }
 
+    public function findOneBy(array $params): array
+    {
+        var_dump($params);
+      
+        foreach ($params as $key => $value) {
+            $where[] = $key . "=:" . $key;
+        }
+        $sql = "SELECT * FROM " . $this->table . " WHERE " . (implode(" AND ", $where));
 
+        /*$queryPrepared = $this->pdo->prepare($sql);
+        $queryPrepared->execute($params);*/
+        $queryPrepared = $this->pdo->prepare($sql, $params);
+
+        return $queryPrepared->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function deleteOne()
+    {
+        if (isset($_POST['id']) && !empty($_POST['id'])) {
+
+            $id = strip_tags($_POST['id']);
+
+            $sql = "DELETE FROM `" . $this->table . "` WHERE `id`=:id";
+          
+        foreach ($params as $key=>$value){
+            $where[] = $key."=:".$key;
+        }
+        $sql = "SELECT * FROM ".$this->table." WHERE ".(implode(" AND ", $where));
+        $queryPrepared = $this->pdo->prepare($sql);
+        $queryPrepared->execute($params);
+       return $queryPrepared->fetch(PDO::FETCH_ASSOC);
     }
 
 }
