a46c3ffd4b63c441367ea9dcee16b61dab318f64
douniaharag <douniaharagsio@gmail.com>
2022-07-18 16:59:08 +0200

    :wrench: Mettre en place un systéme de Log 'Singleton'

---
+++ www/Core/Logger.class.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Logger class
+ * Singleton using lazy instantiation
+ */
+class Logger
+{
+    private static $instance = NULL;
+    private $logs;
+
+    private function __construct() {
+        $logs = array();
+    }
+
+    /**
+     * Gets instance of the Logger
+     * @return Logger instance
+     * @access public
+     */
+    public function getInstance() {
+        if(self::$instance === NULL) {
+            self::$instance = fopen('./errors.log', 'a');
+        }
+        return self::$instance;
+    }
+
+
+    /**
+     * Adds a message to the log
+     * @param String $message Message to be logged
+     * @access public
+     */
+    public function log($message) {
+        $this->logs[] = $message;
+    }
+
+    /**
+     * Returns array of logs
+     * @return array Array of log messages
+     * @access public
+     */
+    public function get_logs() {
+        return $this->logs;
+    }
+
+    public function __destruct()
+    {
+        $instance = self::getInstance();
+        fclose($instance);
+    }
+};
