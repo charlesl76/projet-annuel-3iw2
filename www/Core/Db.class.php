f252fd6bf9f37e0fbd64b822f2357dbb9581320a
Dounia HARAG <59885752+douniaharag@users.noreply.github.com>
2022-07-20 22:13:11 +0200

    📄 Singleton File

--- www/Core/Db.class.php
+++ www/Core/Db.class.php
@@ -3,13 +3,13 @@
 
 	class Db
 	{
-		private $PDOInst = null;
+		private $instance  = null;
 		public static $connection;
 
 		private function __construct()
 		{
 			try{
-                $this->PDOInst = new \PDO(DBDRIVER.":host=".DBHOST.";port=".DBPORT.";dbname=".DBNAME , DBUSER , DBPWD
+                $this->instance  = new \PDO(DBDRIVER.":host=".DBHOST.";port=".DBPORT.";dbname=".DBNAME , DBUSER , DBPWD
                 , [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING]);
             }catch(\Exception $e){
                 die('Error sql' . $e->getMessage());
@@ -33,10 +33,9 @@
 			return self::$connection;
 		}
 
-
-		public function prepare($sql, $array = []){
-			$stmt = $this->PDOInst->prepare($sql);
-			$stmt->execute($array);
-			return $stmt;
+        public function prepare($sql, $array = []){
+			$statement = $this->instance->prepare($sql);
+			$statement->execute($array);
+			return $statement;
 		}
 	}
